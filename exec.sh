ac45d93f525777a37f1440b050ca1d998adf6fff
root <root@ip-172-31-41-250.us-east-2.compute.internal>
2024-02-14 10:39:16 +0000

    changes in shell

--- exec.sh
+++ exec.sh
@@ -2,9 +2,8 @@
 
 docker-compose down
 docker rmi react:dev
-cd /root/lab_docker/react
+cd /var/www/html/react
 docker build -t react:dev .
-
-cd /root/lab_docker/
+cd /var/www/html/react
 docker compose up --detach
 
